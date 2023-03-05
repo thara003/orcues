548b426c4e27b364f2a6251d11b8f3bf9fb9c15e
Yeganathan S <hilariousengineer@protonmail.com>
2023-03-05 20:19:18 +0530

    chore: add basic meta tags and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,35 @@
-import "./globals.css";
+import "../styles/globals.css";
 import { Inter } from "next/font/google";
 import clsx from "clsx";
+import { Metadata } from "next";
 
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
 });
 
-export const metadata = {
-  title: "Orcues",
+export const metadata: Metadata = {
+  title: {
+    default: "Orcues",
+    template: `%s | Orcues`,
+  },
   description: "Redefined news letter subscription flow",
+  keywords: ["Orcues", "News Letter", "Subscription", "Open Source"],
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  icons: {
+    icon: "favicon/favicon.ico",
+    shortcut: "favicon/favicon.ico",
+  },
 };
 
 export default function RootLayout({
