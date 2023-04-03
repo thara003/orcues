1e15c833f8b69802ed4c743f6aec9b03e0fae626
Yeganathan S <hilariousengineer@protonmail.com>
2023-04-03 11:48:49 +0530

    style: user info in settings

--- app/(dashboard)/settings/page.tsx
+++ app/(dashboard)/settings/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import UserInfo from "@/components/app/user-info";
 
 const SettingsPage = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center overscroll-none">
-      <h1 className="text-4xl font-bold text-black">
+      {/* <h1 className="text-4xl font-bold text-black">
         Analytics is under construction.
-      </h1>
+      </h1> */}
+            <UserInfo />
     </div>
   );
 };
