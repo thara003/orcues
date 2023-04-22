40417931e856f382fa1c0946890ddfad8305703e
Yeganathan S <hilariousengineer@protonmail.com>
2023-04-22 19:29:02 +0530

    fix build by removing useRouter on home page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,9 +1,6 @@
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-  router.push("/dashboard");
 
   return (
     <>
