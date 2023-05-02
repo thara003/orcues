972707d1718cc320b45582315aa4b34536f0e3a6
gautamk01 <charmingdeveloper@protonmail.com>
2023-05-02 11:39:50 +0530

    Landing Page finished

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,3 +1,5 @@
+import Footer from "@/components/app/footer";
+import FooterL from "@/components/app/landingfooter";
 import Nav from "@/components/app/nav";
 interface MarketingLayoutProps {
   children: React.ReactNode;
@@ -10,6 +12,7 @@ export default async function MarketingLayout({
     <div className="flex min-h-screen flex-col">
       <Nav />
       <main>{children}</main>
+      <FooterL />
     </div>
   );
 }
