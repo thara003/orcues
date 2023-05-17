1392efe26aa30d7332e934d5ed50ed5e1076d73d
Yeganathan S <hilariousengineer@protonmail.com>
2023-05-17 11:07:28 +0530

    Fetching workspace with `owner_id` example (#16)
    * fetch workspace from supabase using owner_id
    
    * clean unused subscription on useUser hook

--- lib/hooks/useUser.ts
+++ lib/hooks/useUser.ts
@@ -2,8 +2,8 @@ import { useSupabase } from "@/app/supabase-provider";
 import { useEffect, useState } from "react";
 
 type User = {
-  name: string;
   id: string;
+  name: string;
 };
 
 export const useUser = () => {
@@ -12,20 +12,17 @@ export const useUser = () => {
 
   useEffect(() => {
     async function getUser() {
-      const {
-        data: { user },
-      } = await supabase.auth.getUser();
-      if (!user) return setUser(null);
-    }
-
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange(async (event) => {
-      if (event === "SIGNED_OUT") {
+      try {
+        const {
+          data: { user },
+        } = await supabase.auth.getUser();
+        // @ts-ignore
+        setUser(user);
+      } catch (error) {
+        console.error("Error retrieving user:", error.message);
         setUser(null);
       }
-    });
-
+    }
     getUser();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
